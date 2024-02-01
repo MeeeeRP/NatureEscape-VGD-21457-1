e3ebc1e93405d8739b7f048058e3cc034919e358
Maya Perry <mayarp@gmail.com>
2024-02-01 08:07:23 -0500

    puzzle one camera switch

--- unity-tsa-video-game-24/Assets/PuzzleOne Components/SwitchCamera.cs
+++ unity-tsa-video-game-24/Assets/PuzzleOne Components/SwitchCamera.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class SwitchCamera : MonoBehaviour
 {
-    public GameObject Camera_1;
-    public GameObject Camera_2;
+        //This is Main Camera in the Scene
+    public Camera Camera_1;
+    //This is the second Camera and is assigned in inspector
+    public Camera Camera_2;
     public int Manager;
 
+    public PlayerInteractS playerInteract;
+
+
+    public void Start() {
+        //This gets the Main Camera from the Scene
+        Camera_1 = Camera.main;
+        //This enables Main Camera
+        Camera_1.enabled = true;
+        //Use this to disable secondary Camera
+        Camera_2.enabled = false;
+        PlayerInteractS.fairyTalk += ChangeCamera;
+    }
 
     public void ManagerCamera() {
+        print("Manager run");
         if (Manager == 0) {
             Cam_2();
             Manager = 1;
@@ -19,12 +34,19 @@ public class SwitchCamera : MonoBehaviour
         }
     }
     void Cam_1() {
-        Camera_1.SetActive(true);
-        Camera_2.SetActive(false);
+        Camera_1.enabled = true;
+        Camera_2.enabled = false;
     }
 
         void Cam_2() {
-        Camera_2.SetActive(true);
-        Camera_1.SetActive(false);
+        Camera_2.enabled = true;
+        Camera_1.enabled = false;
     }
+
+    public void ChangeCamera() {
+        GetComponent<Animator>().SetTrigger("Change");
+        PlayerInteractS.fairyTalk -= ChangeCamera;
+        print("change trigger");
+    }
+    
 }
